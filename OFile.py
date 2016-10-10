d01e1d493e7f1ad973976084076589a0454eaaad
Michael Huang <bbxxtt2010@gmail.com>
2016-10-10 14:40:09 -0500

    initial commit of file open and write class

---
+++ OFile.py
@@ -0,0 +1,40 @@
+#The output file class#
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+class OFile:
+	
+	def __init__(self, basename):
+		biggest = 0
+		self.basename = basename 
+		files = listdir(os.path.dirname(os.path.abspath(__file__)))
+		onlyfiles = []
+		for f in files:
+			if isfile(join(os.path.dirname(os.path.abspath(__file__)), f)):
+				onlyfiles.append(f)
+
+		for name in onlyfiles:
+			nameparts = name.split('-')
+			if(nameparts[0] == basename):
+				if(nameparts[1] >biggest):
+					biggest = nameparts[1]
+		biggest = biggest+1	
+		self.fileW = open(basename + '-' + biggest, 'w')
+		return self
+
+	#
+	# write() - write the msg to the output file, where #msg is an array consisting of either 
+	#		the msg revcieved by the probe or the msg sent by the beacon
+
+	def write(self, msg):
+		line = ''		
+		for msgElement in msg:
+			line += str(msgElement) + ','
+		line += '\n'
+		self.fileW.write(line)
+		return True
+
+	def close(self):
+		self.fileW.close()
