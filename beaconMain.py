3e5ef82618963675adcac2a23ddacda23cde5bd3
Riya Aggarwal <2000riya@gmail.com>
2016-10-16 16:26:10 -0500

    First test with user input

--- beaconMain.py
+++ beaconMain.py
@@ -2,4 +2,12 @@ from rn4020 import rn4020
 
 
 device = rn4020('COM3')
-device.broadcast(1,2,3,4)
+eNum = raw_input("Enter the experiment nummber: ")
+print eNum
+
+pNum = raw_input("Enter the power: ")
+print pNum
+
+
+#(E#, M#, Sender-Time, Sender-Power)
+device.broadcast(eNum, pNum, 3, 4)
